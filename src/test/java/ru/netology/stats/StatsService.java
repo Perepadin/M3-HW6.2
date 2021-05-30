1359fd75ff12d1cbfa467eab68efb3f846874c9c
perepadin <rabota_1810>
2021-05-30 15:00:10 +0300

    Update2

--- src/test/java/ru/netology/stats/StatsService.java
+++ src/test/java/ru/netology/stats/StatsService.java
@@ -35,7 +35,7 @@ public class StatsService {
 
     public double average(long[] sales) {
         long sum = 0;
-              for (long sale : sales) {
+        for (long sale : sales) {
             sum += sale;
         }
         double average = 0;
